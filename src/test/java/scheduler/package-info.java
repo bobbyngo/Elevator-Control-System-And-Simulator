d8fe137f254cd5408e21486492b442d83c226e63
Zakaria Ismail <1112zakaria@gmail.com>
2023-01-21 00:13:04 -0500

    [ECSS-13] Initial Environment Setup
    **Description**
     - Created Javadoc for Scheduler, Elevator, & Floor subsystem unit test
       packages via package-info.java
    
    **Testing**
    
    **Notes**

---
+++ src/test/java/scheduler/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * This module contains the unit tests for verifying
+ * and validating the functionality of the Scheduler
+ * subsystem in the Elevator Control System & Simulator
+ * @author Zakaria Ismail, 101143497
+ *
+ */
+package test.java.scheduler;
