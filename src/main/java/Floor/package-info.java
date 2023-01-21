b561a28c499b9c1d74160265d33e376340cf0d08
Zakaria Ismail <1112zakaria@gmail.com>
2023-01-20 23:50:07 -0500

    [ECSS-13] Initial Environment Setup
    **Description**
     - Created Javadoc for Scheduler, Elevator, & Floor subsystem packages
       via package-info.java
    
    **Testing**
    
    **Notes**

---
+++ src/main/java/Floor/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * This module contains the software components
+ * forming the Floor subsystem in the Elevator
+ * Control System & Simulator
+ * @author Zakaria Ismail, 101143497
+ *
+ */
+package main.java.Floor;
---
+++ src/main/java/elevator/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * This module contains the software components
+ * forming the Elevator subsystem in the Elevator
+ * Control System & Simulator
+ * @author Zakaria Ismail, 101143497
+ *
+ */
+package main.java.elevator;
