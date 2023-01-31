51d5b861fc9426724e103d91e4b9aeab47863e18
Zakaria Ismail <1112zakaria@gmail.com>
2023-01-30 22:14:20 -0500

    [ECSS-5] Experimental - Elevator class
    **Description**
     - Elevator implementation
    
    **Testing**
    
    **Notes**

---
+++ src/main/java/elevator/Elevator.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package main.java.elevator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import main.java.dto.ElevatorRequest;
+import main.java.scheduler.Scheduler;
+
+/**
+ * Elevator class. Serves elevator requests from the
+ * Scheduler and stores in internal queue
+ * @author Trong Nguyen
+ *
+ */
+public class Elevator implements Runnable {
+	
+	private static final Logger logger = Logger.getLogger(Elevator.class.getName());
+	
+	private int id;
+	private List<ElevatorRequest> requestsQueue;
+	private Scheduler scheduler;
+	
+	/**
+	 * Constructor for Elevator class
+	 * @param id			int, elevator id
+	 * @param scheduler		Scheduler, scheduler object referenced by Elevator
+	 */
+	public Elevator(int id, Scheduler scheduler) {
+		this.id = id;
+		this.scheduler = scheduler;
+		this.requestsQueue = new ArrayList<>();
+	}
+	
+	/**
+	 * Fetch a request from the Scheduler and add to
+	 * requests queue
+	 */
+	public void serveRequest() {
+		ElevatorRequest request;
+		
+		request = scheduler.dispatchRequest();
+		requestsQueue.add(request);
+		logger.info(String.format("Elevator request queued: %s", request));
+		return;
+	}
+
+	/**
+	 * Elevator class' run() implementation.
+	 * Serves requests from the Scheduler until all
+	 * requests have been served.
+	 */
+	@Override
+	public void run() {
+		while (true) {
+			// TODO: add functionality to end when there are no more requests to serve
+			serveRequest();
+		}
+		
+	}
+	
+	
+
+}
