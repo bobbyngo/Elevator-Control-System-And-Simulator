531d7827285007370a70995267a47f8d75bdbad8
trong0dn <55768917+trong0dn@users.noreply.github.com>
2023-02-03 11:48:21 -0500

    Minor refactoring to Scheduler.java

--- src/main/java/scheduler/Scheduler.java
+++ src/main/java/scheduler/Scheduler.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package main.java.scheduler;
 
 import java.util.ArrayList;
@@ -14,10 +11,9 @@ import main.java.dto.ElevatorRequest;
  * Responsible for accepting input from all of the sensors, and
  * sending indications (to devices such as lamps) and commands (to devices such as the motor and door). It is
  * responsible for routing each elevator to requested floors and coordinating elevators in such a way to minimize
- * waiting times for people moving between floors (avoiding starvation)
- * 
+ * waiting times for people moving between floors (avoiding starvation).
  * @author Bobby Ngo
- *
+ * @version 1.0, 02/04/23
  */
 public class Scheduler implements Runnable {
 	
@@ -25,7 +21,7 @@ public class Scheduler implements Runnable {
 	private Logger logger = Logger.getLogger(Scheduler.class.getName());
 
 	/**
-	 * This method is called by the Floor class. The new request will be added to the list of floors to visit
+	 * This method is called by the Floor class. The new request will be added to the list of floors to visit.
 	 * @param elevatorRequest
 	 */
 	public synchronized void putRequest(ElevatorRequest elevatorRequest) {
@@ -39,7 +35,7 @@ public class Scheduler implements Runnable {
 	}
 	
 	/**
-	 * This method will be called by Elevator class. After the floor finished a request it should dispatch an item from the list
+	 * This method will be called by Elevator class. After the floor finished a request it should dispatch an item from the list.
 	 * @return ElevatorRequest
 	 */
 	public synchronized ElevatorRequest dispatchRequest() {
@@ -65,9 +61,9 @@ public class Scheduler implements Runnable {
 	}
 
 	/**
-	 * Scheduler run() method. Sleeps until the process is
-	 * killed.
-	 * @author Zakaria Ismail, 101143497
+	 * Scheduler override run() method. Sleeps until the process is killed.
+	 * @see java.lang.Runnable#run()
+	 * @author Zakaria Ismail
 	 */
 	@Override
 	public void run() {
@@ -75,7 +71,11 @@ public class Scheduler implements Runnable {
 			// Scheduler class is only used for its resource put/get methods
 			// Q: How will the Scheduler thread be used when we distribute our application?
 			Thread.sleep(0);
-		} catch (InterruptedException e) {}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return;
 	}
+	
 }
+
