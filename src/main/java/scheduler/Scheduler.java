f765db48cd977c4e3d8ab39042ceeca59d6fcfd3
Patrick Liu <patrickliu20010610@gmail.com>
2023-02-04 01:14:57 -0500

    synchronization changed to attribute level instead of object level

--- src/main/java/scheduler/Scheduler.java
+++ src/main/java/scheduler/Scheduler.java
@@ -5,8 +5,6 @@ package main.java.scheduler;
 
 import java.util.ArrayList;
 import java.util.logging.*;
-import java.util.Collections;
-import java.util.List;
 
 import main.java.dto.ElevatorRequest;
 
@@ -21,8 +19,8 @@ import main.java.dto.ElevatorRequest;
  */
 public class Scheduler implements Runnable {
 	
-	private List<ElevatorRequest> requestsQueue = Collections.synchronizedList(new ArrayList<>());
-	private List<ElevatorRequest> completedQueue = Collections.synchronizedList(new ArrayList<>());
+	private ArrayList<ElevatorRequest> requestsQueue = new ArrayList<>();
+	private ArrayList<ElevatorRequest> completedQueue = new ArrayList<>();
 	
 	private Logger logger = Logger.getLogger(Scheduler.class.getName());
 
@@ -30,39 +28,47 @@ public class Scheduler implements Runnable {
 	 * This method is called by the Floor class. The new request will be added to the list of floors to visit
 	 * @param elevatorRequest
 	 */
-	public synchronized void putRequest(ElevatorRequest elevatorRequest) {
-		// No duplicate values
-		if (!requestsQueue.contains(elevatorRequest)) {
-			requestsQueue.add(elevatorRequest);
-			logger.info("Added " + elevatorRequest.toString() + " to the request queue. Queue size is: " + requestsQueue.size());
+	public void putRequest(ElevatorRequest elevatorRequest) {
+		
+		synchronized (requestsQueue) {
+			// No duplicate values
+			if (!requestsQueue.contains(elevatorRequest)) {
+				requestsQueue.add(elevatorRequest);
+				logger.info("Added " + elevatorRequest.toString() + " to the request queue. Queue size is: " + requestsQueue.size());
+			}
+			requestsQueue.notifyAll();
 		}
-		notifyAll();
+		
 	}
 	
 	/**
 	 * This method will be called by Elevator class. After the floor finished a request it should dispatch an item from the list
 	 * @return ElevatorRequest
 	 */
-	public synchronized ElevatorRequest dispatchRequest() {
-		while(requestsQueue.size() == 0) {
-			try {
-				logger.info("Waiting for the request");
-				wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				logger.severe(e.getMessage());
-				e.printStackTrace();
+	public ElevatorRequest dispatchRequest() {
+		
+		synchronized (requestsQueue) {
+			while(requestsQueue.size() == 0) {
+				try {
+					logger.info("Waiting for the request");
+					requestsQueue.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					logger.severe(e.getMessage());
+					e.printStackTrace();
+				}
 			}
+			
+			// Iteration 1 we will first come first serve: remove the former index
+			ElevatorRequest removedElevatorRequest = requestsQueue.remove(0);
+			logger.info("Dispatched request " + removedElevatorRequest.toString());
+			logger.info("The queue size is " + requestsQueue.size());
+			
+			requestsQueue.notifyAll();
+			
+			return removedElevatorRequest;
 		}
 		
-		// Iteration 1 we will first come first serve: remove the former index
-		ElevatorRequest removedElevatorRequest = requestsQueue.remove(0);
-		logger.info("Dispatched request " + removedElevatorRequest.toString());
-		logger.info("The queue size is " + requestsQueue.size());
-		
-		notifyAll();
-		
-		return removedElevatorRequest;
 	}
 	
 	/**
@@ -70,30 +76,38 @@ public class Scheduler implements Runnable {
 	 * @param reply		ElevatorRequest, replied elevator request data
 	 * @author Zakaria Ismail, 101143497
 	 */
-	public synchronized void putCompletedRequest(ElevatorRequest reply) {
-		if (!completedQueue.contains(reply)) {
-			completedQueue.add(reply);
-			logger.info(String.format("Added %s to the completed queue. Queue size is %d", reply, completedQueue.size()));
+	public void putCompletedRequest(ElevatorRequest reply) {
+		
+		synchronized (completedQueue) {
+			if (!completedQueue.contains(reply)) {
+				completedQueue.add(reply);
+				logger.info(String.format("Added %s to the completed queue. Queue size is %d", reply, completedQueue.size()));
+			}
+			completedQueue.notifyAll();
 		}
-		notifyAll();
+		
 	}
 	
 	/**
 	 * Gets reply message from the reply queue
 	 * @return		ElevatorRequest, message from the reply queue
 	 */
-	public synchronized ElevatorRequest getCompletedRequest() {
-		ElevatorRequest reply;
+	public ElevatorRequest getCompletedRequest() {
 		
-		while (completedQueue.size() == 0) {
-			try {
-				wait();
-			} catch (InterruptedException e) {}
+		synchronized (completedQueue) {
+			ElevatorRequest reply;
+			
+			while (completedQueue.size() == 0) {
+				try {
+					completedQueue.wait();
+				} catch (InterruptedException e) {}
+			}
+			
+			reply = completedQueue.remove(0);
+			completedQueue.notifyAll();
+			return reply;
 		}
 		
-		reply = completedQueue.remove(0);
-		notifyAll();
-		return reply;
 	}
 
 	/**
