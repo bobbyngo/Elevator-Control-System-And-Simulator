525c854c39bc31d20917b2687dbb9b2bced76ade
koookee <55768904+koookee@users.noreply.github.com>
2023-02-04 18:45:41 -0500

    [ECSS-35] Adding test cases for the Elevator and Floor classes

---
+++ src/test/java/elevator/ElevatorTest.java
@@ -0,0 +1,53 @@
+package test.java.elevator;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.scheduler.Scheduler;
+import main.java.elevator.Elevator;
+
+
+/**
+ * ElevatorTest tests and validates the methods for the
+ * Elevator class using JUnit
+ * @author Hussein El Mokdad
+ * @version 1.0, 02/04/23
+ * @since 1.0, 02/04/23
+ */
+public class ElevatorTest {
+	
+	private Elevator elevator1;
+	private Elevator elevator2;
+	private Elevator elevator3;
+	private Elevator elevator4;
+	private Scheduler scheduler;
+	
+	/**
+	 * Responsible for setting up the test environment 
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+	@Before
+    public void setUp() throws Exception {
+		elevator1 = new Elevator(10, scheduler);
+		elevator2 = new Elevator(1200, scheduler);
+		elevator3 = new Elevator(0, scheduler);
+		elevator4 = new Elevator(123, scheduler);
+    }
+	
+	/**
+	 * Tests getting the elevator id
+	 * 
+	 * @throws IOException
+	 * 
+	 */
+	@Test
+	public void testGetElevatorId() throws IOException {
+		assertEquals(10, elevator1.getElevatorId());
+		assertEquals(1200, elevator2.getElevatorId());
+		assertEquals(0, elevator3.getElevatorId());
+		assertEquals(123, elevator4.getElevatorId());
+	}
+}
